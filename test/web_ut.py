fc235a4b282ccc442fc5b7e1171401b9cb061a5b
Ji Zhi Qian <jizhi.qian@jimubox.com>
2019-12-20 11:13:56 +0800

    Control headless running.

--- test/web_ut.py
+++ test/web_ut.py
@@ -5,6 +5,7 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
 class ISelenium(unittest.TestCase):
@@ -18,28 +19,40 @@ class ISelenium(unittest.TestCase):
 
     def setUp(self):
         config = self.get_config()
-        self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'))
+
+        # 控制是否采用无界面形式运行自动化测试
+        using_headless = os.environ["using_headless"]
+        chrome_options = Options()
+        if using_headless == 'true':
+            chrome_options.add_argument("--headless")
+
+        self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
+                                       chrome_options=chrome_options)
 
     def test_webui_1(self):
         self.driver.get("https://ww.baidu.com")
+        print('Open the browser, visit www.baidu.com')
         time.sleep(2)
         assert u"百度一下" in self.driver.title
 
         self.driver.maximize_window()
+        print("Max the browser window")
 
         elem = self.driver.find_element_by_name("wd")
         elem.send_keys(u"今日头条" + Keys.RETURN)
         time.sleep(2)
-        assert u"今日头条" in self.driver.title
+        self.assertTrue(u"今日头条" in self.driver.title, msg='Verify test webui_1 pass')
 
     def test_webui_2(self):
         self.driver.get("https://ww.baidu.com")
+        print('Open the browser, visit www.baidu.com')
         time.sleep(2)
         assert u"百度一下" in self.driver.title
 
         self.driver.maximize_window()
+        print("Max the browser window")
 
         elem = self.driver.find_element_by_name("wd")
         elem.send_keys(u"王者荣耀" + Keys.RETURN)
         time.sleep(2)
-        assert u"王者荣耀" in self.driver.title
+        self.assertTrue(u"王者荣耀" in self.driver.title, msg='Verify test webui_2 pass')
