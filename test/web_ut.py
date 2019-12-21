b5ce3842494ac3fd84c7c37790a932fe72c739be
Ji Zhi Qian <jizhi.qian@jimubox.com>
2019-12-21 11:23:21 +0800

    To zh

--- test/web_ut.py
+++ test/web_ut.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 class ISelenium(unittest.TestCase):
+    # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
         config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
@@ -24,6 +25,7 @@ class ISelenium(unittest.TestCase):
         using_headless = os.environ["using_headless"]
         chrome_options = Options()
         if using_headless == 'true':
+            print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
 
         self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
@@ -31,28 +33,30 @@ class ISelenium(unittest.TestCase):
 
     def test_webui_1(self):
         self.driver.get("https://ww.baidu.com")
-        print('Open the browser, visit www.baidu.com')
+        print('打开浏览器，访问 www.baidu.com')
         time.sleep(2)
         assert u"百度一下" in self.driver.title
 
         self.driver.maximize_window()
-        print("Max the browser window")
+        print("浏览器窗口最大化")
 
         elem = self.driver.find_element_by_name("wd")
         elem.send_keys(u"今日头条" + Keys.RETURN)
+        print('搜索关键词~今日头条')
         time.sleep(2)
-        self.assertTrue(u"今日头条" in self.driver.title, msg='Verify test webui_1 pass')
+        self.assertTrue(u"今日头条" in self.driver.title, msg='webui_1校验点 pass')
 
     def test_webui_2(self):
         self.driver.get("https://ww.baidu.com")
-        print('Open the browser, visit www.baidu.com')
+        print('打开浏览器，访问 www.baidu.com')
         time.sleep(2)
         assert u"百度一下" in self.driver.title
 
         self.driver.maximize_window()
-        print("Max the browser window")
+        print("浏览器窗口最大化")
 
         elem = self.driver.find_element_by_name("wd")
         elem.send_keys(u"王者荣耀" + Keys.RETURN)
+        print('搜索关键词~王者荣耀')
         time.sleep(2)
-        self.assertTrue(u"王者荣耀" in self.driver.title, msg='Verify test webui_2 pass')
+        self.assertTrue(u"王者荣耀" in self.driver.title, msg='webui_1校验点 pass')
